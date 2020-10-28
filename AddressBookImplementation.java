16a15f29ed312713b8b9907d6e80f19d609a7eb6
Komal-8080 <shindeg163@gmail.com>
2020-10-29 01:19:04 +0530

    Methods for AddressBookInterface implemented

---
+++ AddressBookImplementation.java
@@ -0,0 +1,46 @@
+package addressbook;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class AddressBookImplementation implements AddressBookInterface {
+
+	 	public void addPerson() {
+
+
+		}
+		public void editPerson() {
+
+
+     	  	}
+
+
+		public void deletePerson() {
+
+
+		}
+
+
+		public void sortByName() {
+
+
+		}
+
+
+		public void sortByZip() {
+
+
+		}
+
+		public void searchPerson() {
+
+
+		}
+
+
+		public void display(){
+
+
+
+		}
+}
