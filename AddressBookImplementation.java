f838a959a8bb1ab33167d092c25e2e795c0cadf0
Komal-8080 <shindeg163@gmail.com>
2020-11-03 00:32:21 +0530

    Functionality to Edit Person Details and save in Array list completed

--- AddressBookImplementation.java
+++ AddressBookImplementation.java
@@ -30,6 +30,66 @@ public class AddressBookImplementation implements AddressBookInterface {
 
 		public void editPerson() {
 
+			System.out.println("Enter person First Name to Edit details");
+			Scanner toedit = new Scanner(System.in);
+			String firstName = toedit.nextLine();
+			boolean exists = false;
+
+			  for (Person p:personData) {
+				  System.out.println(p.toString());
+
+					  if (firstName.equals(p.firstName)) {
+						  exists = true;
+
+						  while (exists == true) {
+						  System.out.println("Select an option to edit\n" 
+								     +"1] Phone Number\n"
+								     +"2] City\n"
+								     +"3] State\n"
+								     +"4] ZipCode\n"
+								     +"5] Quit");
+
+						  		Scanner option = new Scanner(System.in);
+						  		int num = option.nextInt();
+						  				switch (num) {							  					case 1:
+						  						System.out.println("Enter New Phone Number");
+						  						String phone = option.nextLine();
+						  						p.setPhone(phone);
+						  						System.out.println(p.toString());
+						  							break;
+						  					case 2:
+						  						System.out.println("Enter New City Name");
+						  						String city = option.nextLine();
+						  						p.setCity(city);
+						  						System.out.println(p.toString());
+						  							break;
+						  					case 3:
+						  						System.out.println("Enter New State Name");
+						  						String state = option.nextLine();
+						  						p.setState(state);
+						  						System.out.println(p.toString());
+						  							break;
+						  					case 4:
+						  						System.out.println("Enter New Zip Code");
+						  						String zip = option.nextLine();
+						  						p.setZip(zip);
+						  						System.out.println(p.toString());
+						  							break;
+						  					case 5:
+						  						exists = false;
+						  							break;
+						  					default :
+						  						System.out.println("Invalid input");
+						  							break;
+						  				}
+						  		}
+					  		}
+
+					  else {
+						  			System.out.println("Record Not Found");
+
+					  	}
+			        }
 
      	  	}
 
