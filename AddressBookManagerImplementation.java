35a822f90ee179b083256279c7f76c4cb56b3af1
Komal-8080 <shindeg163@gmail.com>
2020-11-03 00:08:48 +0530

    Empty Methods for AddressBookManagerInterface implemented

---
+++ AddressBookManagerImplementation.java
@@ -0,0 +1,36 @@
+package addressbook;
+
+public class AddressBookManagerImplementation implements AddressBookManagerInterface{
+	AddressBookImplementation addressBookImplementation = new AddressBookImplementation();
+
+	public void newAddressBook() {
+
+
+	}
+
+
+	public void openAddressBook() {
+
+
+	}
+
+
+	public void saveAddressBook() {
+
+
+	}
+
+
+	public void saveAsAddressBook() {
+
+
+	}
+
+
+
+	public void closeAddressBook() {
+
+
+	}
+}
+
