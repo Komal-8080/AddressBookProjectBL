f20690c841452ebedf11cc3e27605e42c90ba105
Komal-8080 <shindeg163@gmail.com>
2020-11-06 17:35:59 +0530

    Open Address Book Method Modified to do functionalities using CSV File

--- AddressBookManagerImplementation.java
+++ AddressBookManagerImplementation.java
@@ -53,46 +53,73 @@ import java.util.Scanner;
 
 		public void openAddressBook() {
 
-			int openAb=0;
-			while (openAb == 0) {
+		    int ablist = 0;
+			while (ablist == 0) {
+				System.out.println("1] To Open Address Book List\n" + "2] Back to Menu ");
+				Scanner value = new Scanner(System.in);
+				int in = value.nextInt();
+					addressBookImplementation.viewAddressBookList();
+ 						switch (in) {
+ 						case 1:
+ 							System.out.println("");
+ 							System.out.println("Enter Address Book Name you want to open");
+ 							addressBookImplementation.readData();
+								int openAb=0;
+								while (openAb == 0) {
+									System.out.println("");
+									System.out.println("1.Add Person\n" + "2.Edit Person\n" + "3.Delete Person\n" + "4.Sort By Name\n"
+											   +"5.Sort By Zip\n" + "6.Search Person\n" + "7.Display\n" + "8.Quit");
+									System.out.println("Select from above Options");
+									Scanner num = new Scanner(System.in);
+									int selectedValue = num.nextInt();
 
-					System.out.println("");
-					System.out.println("1.Add Person\n" + "2.Edit Person\n" + "3.Delete Person\n" + "4.Sort By Name\n"
-									   +"5.Sort By Zip\n" + "6.Search Person\n" + "7.Display\n" + "8.Quit");
-					System.out.println("Select from above Options");
-					Scanner num = new Scanner(System.in);
-					int selectedValue = num.nextInt();
+							             switch (selectedValue) {
+									case 1:
+									      System.out.println("Enter Person Details");
+									      addressBookImplementation.addPerson();
+									      System.out.println("");
+ 									      System.out.println("Enter Address Book name to save changes");
+ 									      addressBookImplementation.writeData();
+ 									      System.out.println("*****Person Details Saved Successfully*****");
+										break;
+									case 2:
+									      System.out.println("");
+									      addressBookImplementation.editPerson();
+									 	break;
+									case 3:
+									      System.out.println("");
+									      addressBookImplementation.deletePerson();
+										break;
+									case 4:
+									      addressBookImplementation.sortByName();
+										break;
+									case 5:
+								              addressBookImplementation.sortByZip();
+										break;
+									case 6:
+									      addressBookImplementation.searchPerson();
+										break;
+									case 7:
+									      System.out.println("");
+									      addressBookImplementation.display();
+										break;
+									case 8:
+										openAb=1;
+										break;
+									default:
+										System.out.println("Invalid Option");
+										break;
+							              }
+					  		}
 
-						switch (selectedValue) {
-							case 1:
-								addressBookImplementation.addPerson();
-									break;
-							case 2:
-								addressBookImplementation.editPerson();
-									 break;
-							case 3:
-								addressBookImplementation.deletePerson();
-									break;
-							case 4:
-								addressBookImplementation.sortByName();
-									break;
-							case 5:
-								addressBookImplementation.sortByZip();
-									break;
-							case 6:
-								addressBookImplementation.searchPerson();
-									break;
-							case 7:
-								addressBookImplementation.display();
-									break;
-							case 8:
-								openAb=1;
-									break;
-							default:
-								System.out.println("Invalid Option");
-									break;
-							}
-					}
+						case 2:
+ 							ablist = 1;
+ 								break;
+ 						default:
+							System.out.println("Invalid Option");
+								break;
+						}
+				}
 		}
 
 
